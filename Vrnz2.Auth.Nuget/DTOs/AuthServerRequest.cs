94e5a24d5c5543bf698c9e8932de3074efed8f0c
Vernizze <cfvernizze@gmail.com>
2020-12-27 20:53:33 -0300

    Ajuste na request

--- Vrnz2.Auth.Nuget/DTOs/AuthServerRequest.cs
+++ Vrnz2.Auth.Nuget/DTOs/AuthServerRequest.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using Vrnz2.BaseContracts.DTOs.Base;
 
 namespace Vrnz2.Auth.Nuget.DTOs
 {
     public class AuthServerRequest
+        : BaseDTO.Request
     {
         public string token { get; set; }
         public List<string> claims { get; set; }
