5546bf9bad95ffd5955fb928eb4de86ef44a4790
Vernizze <cfvernizze@gmail.com>
2020-12-28 06:42:03 -0300

    Criacao do validador da request 'AuthServerRequest'

---
+++ Vrnz2.Auth.Nuget/Validations/AuthServerRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Vrnz2.Auth.Nuget.DTOs;
+
+namespace Vrnz2.Auth.Nuget.Validations
+{
+    public class AuthServerRequestValidator
+        : AbstractValidator<AuthServerRequest>
+    {
+        #region Constructors
+
+        public AuthServerRequestValidator()
+        {
+            RuleFor(v => v.token)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Token invalid!");//TODO: Arrumar!
+        }
+
+        #endregion
+    }
+}
